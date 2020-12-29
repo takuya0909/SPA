a9496bae3a0469a18b474d8d51d584c65d390d26
takya0909 <takuya19940909@yahoo.co.jp>
2020-12-29 11:40:50 +0900

    タスク登録コンポーネント実装

---
+++ laravel/resources/js/components/TaskCreateComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <form action="">
+        <div class="form-group row">
+          <label for="title"></label>
+          <input type="text" class="col-sm-9 form-control" id="title">
+        </div>
+        <div class="form-group row">
+          <label for="content"></label>
+          <input type="text" class="col-sm-9 form-control" id="content">
+        </div>
+        <div class="form-group row">
+          <label for="person-in-charge"></label>
+          <input type="text" class="col-sm-9 form-control" id="person-in-charge">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+  export default {}
+</script>
